7124a9da4d2bfdfa2392d3d5cac0e5f6df55ac44
Markus Janik <mj@eurodata.co.at>
2021-12-01 17:30:07 +0100

    improved day1

--- src/main/java/day1/Part2.java
+++ src/main/java/day1/Part2.java
@@ -5,15 +5,14 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Part2 {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         Path input = Paths.get(Part1.class.getResource("/day1.txt").toURI());
-        var list = new ArrayList<Integer>();
-        Files.lines(input).forEach(x -> list.add(Integer.parseInt(x)));
+        var list = Files.readAllLines(input).stream().map(Integer::parseInt).collect(Collectors.toList());
         int counter = (int) IntStream.range(0, list.size() - 3).filter(i -> list.get(i)+list.get(i+1)+list.get(i+2) < list.get(i+1)+list.get(i+2)+list.get(i+3)).count();
         System.out.println(counter);
     }
