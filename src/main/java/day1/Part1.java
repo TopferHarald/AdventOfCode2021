b6c655154f4aff2cee3171a52ee0bf1c8423181d
Markus Janik <mj@eurodata.co.at>
2021-12-01 17:50:10 +0100

    improved day1

--- src/main/java/day1/Part1.java
+++ src/main/java/day1/Part1.java
@@ -5,13 +5,14 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Part1 {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        Path input = Paths.get(Part1.class.getResource("/day1.txt").toURI());
+        Path input = Paths.get(Objects.requireNonNull(Part1.class.getResource("/day1.txt")).toURI());
         var list = Files.readAllLines(input).stream().map(Integer::parseInt).collect(Collectors.toList());
         int counter = (int) IntStream.range(0, list.size() - 1).filter(i -> list.get(i) < list.get(i + 1)).count();
         System.out.println(counter);
--- src/main/java/day1/Part2.java
+++ src/main/java/day1/Part2.java
@@ -5,15 +5,16 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Part2 {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        Path input = Paths.get(Part1.class.getResource("/day1.txt").toURI());
+        Path input = Paths.get(Objects.requireNonNull(Part2.class.getResource("/day1.txt")).toURI());
         var list = Files.readAllLines(input).stream().map(Integer::parseInt).collect(Collectors.toList());
-        int counter = (int) IntStream.range(0, list.size() - 3).filter(i -> list.get(i)+list.get(i+1)+list.get(i+2) < list.get(i+1)+list.get(i+2)+list.get(i+3)).count();
+        int counter = (int) IntStream.range(0, list.size() - 3).filter(i -> list.get(i) < list.get(i+3)).count();
         System.out.println(counter);
     }
 }
