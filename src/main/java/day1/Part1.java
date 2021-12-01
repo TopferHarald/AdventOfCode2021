1d847ed0b8542b3faea44cf3f7455a3c33c96551
Markus Janik <mj@eurodata.co.at>
2021-12-01 17:11:32 +0100

    init day1

--- src/main/java/day1/Part1.java
+++ src/main/java/day1/Part1.java
@@ -8,13 +8,13 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.stream.IntStream;
 
-public class Solution2 {
+public class Part1 {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        Path input = Paths.get(Solution1.class.getResource("/day1.txt").toURI());
+        Path input = Paths.get(Part1.class.getResource("/day1.txt").toURI());
         var list = new ArrayList<Integer>();
         Files.lines(input).forEach(x -> list.add(Integer.parseInt(x)));
-        int counter = (int) IntStream.range(0, list.size() - 3).filter(i -> list.get(i)+list.get(i+1)+list.get(i+2) < list.get(i+1)+list.get(i+2)+list.get(i+3)).count();
+        int counter = (int) IntStream.range(0, list.size() - 1).filter(i -> list.get(i) < list.get(i + 1)).count();
         System.out.println(counter);
     }
 }
